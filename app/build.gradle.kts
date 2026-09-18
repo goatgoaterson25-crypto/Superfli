65c428b5d9f61a6f3cadae93febaf61256f069a2
goatgoaterson25-crypto <goatgoaterson25@gmail.com>
2026-09-18 14:03:14 -0500

    Apollo: Injected build config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.apollo.missioncontrol"
+    namespace = "com.apollo.Superfli"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.apollo.missioncontrol"
+        applicationId = "com.apollo.Superfli"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
